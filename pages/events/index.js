e45434fa6b6d688b69f6a0531e50e50461f03f28
Sanzay Dahal <sanzaydahal12345@gmail.com>
2023-07-09 15:03:33 +1000

    Add components, layout, pages, styles and more

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,21 @@
+import { useRouter } from 'next/router';
+import { getAllEvents } from '@/data/dummy-data';
+import EventList from '@/components/events/event-list';
+import EventsSearch from '@/components/events/events-search';
+
 function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
   return (
     <div>
-      <h1>The AllEvents Page</h1>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
     </div>
   );
 }
