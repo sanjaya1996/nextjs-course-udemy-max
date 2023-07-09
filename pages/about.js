870ec5065f96796e6741e2b66d80fdf0fb0d1abe
Sanzay Dahal <sanzaydahal12345@gmail.com>
2023-07-09 12:56:29 +1000

    Initial Setup for Project

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-function AboutPage() {
-  return (
-    <div>
-      <h1>The About page</h1>
-    </div>
-  );
-}
-
-export default AboutPage;
--- pages/portfolio/list.js
+++
@@ -1,9 +0,0 @@
-function ListPage() {
-  return (
-    <div>
-      <h1>The List page</h1>
-    </div>
-  );
-}
-
-export default ListPage;
