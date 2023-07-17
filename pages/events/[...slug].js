493b2d2cf2ac7ed30adcf9f51eaaa10af09d33f6
Sanzay Dahal <sanzaydahal12345@gmail.com>
2023-07-17 23:44:18 +1000

    Working with All Events Page and Using Server-side Rendering

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -2,15 +2,64 @@ import EventList from '@/components/events/event-list';
 import ResultsTitle from '@/components/events/results-title';
 import Button from '@/components/ui/button';
 import ErrorAlert from '@/components/ui/error-alert';
-import { getFilteredEvents } from '@/data/dummy-data';
+import { getFilteredEvents } from '@/helpers/api-util';
 import { useRouter } from 'next/router';
 
-function FilteredEventsPage() {
-  const router = useRouter();
-  const filterData = router.query.slug;
-  if (!filterData) {
-    return <p className='center'>Loading....</p>;
+function FilteredEventsPage(props) {
+  // const router = useRouter();
+  // const filterData = router.query.slug;
+  // if (!filterData) {
+  //   return <p className='center'>Loading....</p>;
+  // }
+
+  // const filteredYear = filterData[0];
+  // const filteredMonth = filterData[1];
+
+  // const numYear = +filteredYear;
+  // const numMonth = +filteredMonth;
+
+  if (props.hasError) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>;
+          <div className='center'>
+            <Button link='/events'>Show All Events</Button>
+          </div>
+        </ErrorAlert>
+      </>
+    );
   }
+
+  const filteredEvents = props.events;
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for the choosen filters</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </>
+    );
+  }
+
+  const date = new Date(props.date.year, props.date.month - 1);
+  return (
+    <div>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </div>
+  );
+}
+
+export default FilteredEventsPage;
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const filterData = params.slug;
 
   const filteredYear = filterData[0];
   const filteredMonth = filterData[1];
@@ -26,40 +75,23 @@ function FilteredEventsPage() {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return (
-      <>
-        <ErrorAlert>
-          <p>Invalid filter. Please adjust your values!</p>;
-          <div className='center'>
-            <Button link='/events'>Show All Events</Button>
-          </div>
-        </ErrorAlert>
-      </>
-    );
+    return {
+      props: { hasError: true },
+      // notFound: true,
+      // redirect: { destination: '/error' },
+    };
   }
 
-  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+  const filteredEvents = await getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
 
-  if (!filteredEvents || filteredEvents.length === 0) {
-    return (
-      <>
-        <ErrorAlert>
-          <p>No events found for the choosen filters</p>;
-        </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
-        </div>
-      </>
-    );
-  }
-
-  const date = new Date(numYear, numMonth - 1);
-  return (
-    <div>
-      <ResultsTitle date={date} />
-      <EventList items={filteredEvents} />
-    </div>
-  );
+  return {
+    props: {
+      events: filteredEvents,
+      date: { year: numYear, month: numMonth },
+      hasError: false,
+    },
+  };
 }
-
-export default FilteredEventsPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,11 @@
 import { useRouter } from 'next/router';
-import { getAllEvents } from '@/data/dummy-data';
 import EventList from '@/components/events/event-list';
 import EventsSearch from '@/components/events/events-search';
+import { getAllEvents } from '@/helpers/api-util';
 
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const events = props.events;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -21,3 +21,8 @@ function AllEventsPage() {
 }
 
 export default AllEventsPage;
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return { props: { events }, revalidate: 60 };
+}
